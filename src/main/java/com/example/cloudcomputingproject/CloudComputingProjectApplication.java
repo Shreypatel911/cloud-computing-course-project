abc7559d574f41531db2c78a29b67c797e7478e2
Shreypatel911 <77115893+Shreypatel911@users.noreply.github.com>
2024-02-07 16:08:26 -0700

    initial project commit

---
+++ src/main/java/com/example/cloudcomputingproject/CloudComputingProjectApplication.java
@@ -0,0 +1,11 @@
+package com.example.cloudcomputingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudComputingProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CloudComputingProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/cloudcomputingproject/CloudComputingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudcomputingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudComputingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
