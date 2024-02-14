908cfcef16a6e7e8b43a6ecb0c235bb87b055cb7
Shreypatel911 <77115893+Shreypatel911@users.noreply.github.com>
2024-02-14 14:34:56 -0700

    add aws integration

--- src/main/java/com/example/cloudcomputingproject/controller/InputController.java
+++ src/main/java/com/example/cloudcomputingproject/controller/InputController.java
@@ -2,14 +2,14 @@ package com.example.cloudcomputingproject.controller;
 
 import com.example.cloudcomputingproject.service.FileReader;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
-@Controller
+@RestController
 public class InputController {
     private final FileReader fileReader;
 
@@ -25,11 +25,8 @@ public class InputController {
 
         String fileName = file.getOriginalFilename();
         fileName = fileName.substring(0, fileName.length() - 4);
-        String filePath100 = "src/main/resources/static/Classification Results on Face Dataset (100 images).csv";
-        String filePath1000 = "src/main/resources/static/Classification Results on Face Dataset (1000 images).csv";
 
-        Map<String, String> imageToNames100 = fileReader.excelReader(filePath100);
-//        Map<String, String> imageToNames1000 = fileReader.excelReader(filePath1000);
+        Map<String, String> imageToNames100 = fileReader.excelReader();
 
         return ResponseEntity.ok(fileName + ":" + imageToNames100.get(fileName));
     }
--- src/main/java/com/example/cloudcomputingproject/service/FileReader.java
+++ src/main/java/com/example/cloudcomputingproject/service/FileReader.java
@@ -2,17 +2,19 @@ package com.example.cloudcomputingproject.service;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 @Service
 public class FileReader {
-    public Map<String, String> excelReader(String filePath)  {
-        Map<String, String> imageToNames = new TreeMap<>();
+    private final String filePath = "/home/ec2-user/Classification Results on Face Dataset (1000 images).csv";
+    private final Map<String, String> imageToNames = new TreeMap<>();
+
+    @PostConstruct
+    public void init(){
         try (CSVReader reader = new CSVReader(new java.io.FileReader(filePath))) {
             String[] nextLine;
             while ((nextLine = reader.readNext()) != null) {
@@ -27,7 +29,8 @@ public class FileReader {
         } catch (CsvValidationException e) {
             throw new RuntimeException(e);
         }
-
+    }
+    public Map<String, String> excelReader()  {
         return imageToNames;
     }
 }
